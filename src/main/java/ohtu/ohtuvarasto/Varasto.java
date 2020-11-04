1d99d97ecf42b7a49363509863b4ce06b093c0c2
jmlii <44909874+jmlii@users.noreply.github.com>
2020-11-04 21:12:15 +0200

    Korjattu varastosta checkstylen rike

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -22,9 +22,9 @@ public class Varasto {
         } else { // virheellinen, nollataan
             this.tilavuus = 0.0;  // => käyttökelvoton varasto
         }
-        if (alkuSaldo < 0.0) {
-            this.saldo = 0.0;
-        } else if (alkuSaldo <= tilavuus) { // mahtuu
+//        if (alkuSaldo < 0.0) {
+//            this.saldo = 0.0;
+        if (alkuSaldo <= tilavuus) { // mahtuu
             this.saldo = alkuSaldo;
         } else {
             this.saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
